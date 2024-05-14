af97559dcd52a1b754bc05b5460df74ec41f86e7
NoobZFardeen <alohafardeen@gmail.com>
2024-05-14 00:53:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. NoobZ CI/CD
+          Edit <code>src/App.js</code> and save to reload. NoobZ CI/CD Hrishi
         </p>
         <a
           className="App-link"
